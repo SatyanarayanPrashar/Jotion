524b90f571dae80ce8e379c89484e512856b6c6b
Satyanarayan Prashar <satyanarayanprashar73@gmail.com>
2024-02-13 00:45:57 +0530

    search done

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -14,8 +14,10 @@ import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
+import { useSearch } from "@/hooks/use-search";
 
 export const Navigation = () => {
+    const search = useSearch();
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
     const create = useMutation(api.documents.create);
@@ -134,7 +136,7 @@ export const Navigation = () => {
                 <div>
                     <UserItem />
                     <Item 
-                        onClick={()=>{}}
+                        onClick={search.onOpen}
                         label="Search"
                         icon={Search}
                         isSearch
